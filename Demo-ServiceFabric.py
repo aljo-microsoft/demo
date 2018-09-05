6f1f9ed0e55df0409313e4d93d9ce0b575c752c9
aljo-microsoft <aljo@microsoft.com>
2018-09-05 15:18:48 -0700

    updated Demo files

--- Demo-ServiceFabric.py
+++ Demo-ServiceFabric.py
@@ -1,92 +1,36 @@
-import subprocess
-import json
+from ServiceFabric-ResourceProperties import ResourceProperties
 
-class Resources:
+# TODO: Include az client validate or install logic
+# Initialize RM Client with valid deployment properties
+# HLT: ARM, Az cli, and minimum set of SF properties
+rp = ResourceProperties()
 
-	def __init__(self, subscription="eec8e14e-b47d-40d9-8bd9-23ff5c381b40", template_file="AzureDeploy.json", parameters_file="AzureDeploy.Parameters.json", resource_group="MyDeploymentResourceGroup", clusterName="MyCluster", adminUserName="myAdmin", adminPassword="Password#1234"):
-		self.clusterName = clusterName
+# Create Keyvault Self Signed Cert and update deployment cert properties
+# HLT: Thumbprint pain of updating template, and keyvault - I.E. Current State
+rp.createCertificate()
 
-		# Az CLI Client	
-		self.subscription = subscription
+# Create Service Fabric Cluster
+# HLT: Minimum resource Types to create cluster: VMSS, Network, Storage, ect
+rp.createCluster()
 
-		loginCmd = 'az login'
-		accountSetCmd = 'az account set --subscription " + self.subscription
-		cmds = loginCmd + ";" accountSetCmd
-		subprocess.call(cmds, shell=True)
+# TODO: SFRP demo steps
+# clientSetup() - download cert, convert to proper format
+# HLT: sfctl deployment - I.E. on prim and administration
+# patchOrchestrationApplication() - create storage account, copy POA to storage, declare as resource, and deploy POA 
+# HLT: App/Svc as resources
+# enableHostMSI()
+# HLT: MSI today and why it is important
+# hostMSIPermissions(resourceName, permissions)
+# HLT: policies grant permissions to resources
+# deployNativeDemoApp() - Go guest exe, authenticate to Cosmos DB with MSI to write data
+# HLT: app without secrets authenticates to cosmos DB - value add of MSI
 
-		# Validate Template and Parms Syntax
-		self.template_file = template_file
-		self.parameters_file = parameters_file
+# TODO: Mesh demo of Secure Store Service
+# enableSecureStoreService()
+# HLT: Feature announcement and how to enable it
+# deployMeshDemoSecrets(acrSecretName, acrSecretValue) - password for ACR
+# HLT: Management operations of 3S 
+# deployMeshDemoApp() - update package, deploy containerized App, use 3S secure password for ACR, and MSI to write data to DB.
+# HLT: 3S to secure secrets, and MSI to avoid manual handling of secrets
 
-		self.parameters_file_json = json.load(open(self.parameters_file))
-
-		self.location = parameters_file_json['parameters']['location']['value']
-		
-		validateCmd = 'az group deployment validate --resource-group ' + resource_group + ' --mode Incremental --parameters ' + parameters_file_json['parameters'] + ' --template-file ' + self.template_file'
-
-		subprocess.call(validateCmd, shell=True)
-
-		# Set Parameter values
-		self.parameters_file_json['parameters']['clusterName']['value'] = self.clusterName
-		self.parameters_file_json['parameters']['adminUserName']['value'] = adminUserName
-		self.parameters_file_json['parameters']['adminPassword']['value'] = adminPassword
-
-	def selfSignedCertificate(self, keyVaultName="MyKeyVault", keyVaultResourceGroup="MyKeyVaultResourceGroup", certificateName="MySelfSignedCertificate", description="My Self Signed Certificate", certificatePassword="Password#1234"):
-		
-		self.description = description
-		self.keyVaultName = keyVaultName
-		self.keyVaultResourceGroup = keyVaultResourceGroup
-		self.certificateName = certificateName
-		self.certificatePassword = certificatePassword
-
-		# Make KeyVault
-		groupCreateCmd = 'az group create --name ' + self.keyVaultResourceGroup + ' --location ' + self.location
-		keyVaultCreateCmd = 'az keyvault create --name ' + self.keyVaultName + ' --resource-group ' + self.keyVaultResourceGroup
-		cmds = groupCreateCmd + ';' + keyVaultCreateCmd
-		subprocess.call(cmds, shell=True)
-
-		# KeyVault DNS Population 10 secs
-		keyVaultCreateValidationCmd = 'az keyvault show -n ' + self.keyVaultName + ' -g ' + self.keyVaultResourceGroup
-		subprocess.call(keyVaultCreateValidateCmd, shell=True)
-
-		# Create Self Signed Certificate
-		self.dnsName = self.clusterName + '.' + self.location + '.cloudapp.azure.com'
-		certificateCreateCmd = "openssl req -x509 -newkey rsa:2048 -subj '/CN='" + self.dnsName + " -days 365 -out " + self.dnsName + ".crt -keyout " + self.dnsName + ".pem -passout pass:" + self.certificatePassword
-
-		subprocess.call(certificateCreateCmd, shell=True)
-
-		# Convert PEM to PFX
-		convertCertificateCmd = "openssl pkcs12 -export -in " + self.dnsName + ".crt -inkey " + self.dnsName + ".pem -passin pass:" + self.certificatePassword + " -out " + self.dnsName + ".pfx -passout pass:" + self.certificatePassword
-
-		subprocess.call(convertCertificateCmd, shell=True)
-
-		self.PFXCertificate = self.dnsName + '.pfx'
-
-		# Upload Certificate to KeyVault
-		uploadCertificateCmd = "az keyvault secret set --description '" + self.description + "' --encoding base64 --file ./" + self.PFXCertificate + " --name " + self.certificateName + " --vault-name " + self.keyVaultName
-
-		subprocess.call(uploadCertificateCmd, shell=True)
-
-		# Update Resource ID Parameter
-		resourceIdProcess = subprocess.Popen(["az", "keyvault", "show", "--name", self.keyVaultName, "--query", "id", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-		stdout, stderr = resourceIdProcess.communicate()
-
-		self.parameters_file_json['parameters']['sourceVaultValue']['value'] = stdout.decode("utf-8")
-
-		# Update Thumbprint Parameter
-		thumbprintProcess = subprocess.Popen(["az", "keyvault", "secret", "--vault-name", self.keyVaultName, "--name", self.certificateName, "--query", "x509ThumbprintHex", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-		stdout, stderr = thumbprintProcess.communicate()
-
-		self.parameters_file_json['parameters']['certificateThumbprint']['value'] = stdout.decode("utf-8")
-
-		# Update URL Parameter
-		urlProcess = subprocess.Popen(["az", "keyvault", "secret", "show", "--vault-name", self.keyVaultName, "--name", self.certificateName, "--query", "id", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-		stdout, stderr = urlProcess.communicate()
-
-		self.parameters_file_json['parameters']['certificateUrlValue']['value'] = stdout.decode("utf-8")
-
-		# Update Parameters File
-		json.dump(self.parameters_file_json, open(self.parameters_file, 'w')) 
+# TODO: Stretch goal is to get letsencrypt certificate deployment
---
+++ ServiceFabric-ResourceProperties.py
@@ -0,0 +1,91 @@
+import subprocess
+import json
+
+class ResourceProperties:
+
+	def __init__(self, subscription="eec8e14e-b47d-40d9-8bd9-23ff5c381b40", template_file="AzureDeploy.json", parameters_file="AzureDeploy.Parameters.json", resource_group="MyDeploymentResourceGroup", clusterName="MyCluster", adminUserName="myAdmin", adminPassword="Password#1234"):
+		self.clusterName = clusterName
+
+		# Az CLI Client	
+		self.subscription = subscription
+
+		loginCmd = 'az login'
+		accountSetCmd = 'az account set --subscription " + self.subscription
+		cmds = loginCmd + ";" accountSetCmd
+		subprocess.call(cmds, shell=True)
+
+		# Validate Template and Parms Syntax
+		self.template_file = template_file
+		self.parameters_file = parameters_file
+
+		self.parameters_file_json = json.load(open(self.parameters_file))
+
+		self.location = parameters_file_json['parameters']['location']['value']
+		
+		validateCmd = 'az group deployment validate --resource-group ' + resource_group + ' --mode Incremental --parameters ' + parameters_file_json['parameters'] + ' --template-file ' + self.template_file'
+
+		subprocess.call(validateCmd, shell=True)
+
+		# Set Parameter values
+		self.parameters_file_json['parameters']['clusterName']['value'] = self.clusterName
+		self.parameters_file_json['parameters']['adminUserName']['value'] = adminUserName
+		self.parameters_file_json['parameters']['adminPassword']['value'] = adminPassword
+
+	def selfSignedCertificate(self, keyVaultName="MyKeyVault", keyVaultResourceGroup="MyKeyVaultResourceGroup", certificateName="MySelfSignedCertificate", description="My Self Signed Certificate", certificatePassword="Password#1234"):
+		
+		self.description = description
+		self.keyVaultName = keyVaultName
+		self.keyVaultResourceGroup = keyVaultResourceGroup
+		self.certificateName = certificateName
+		self.certificatePassword = certificatePassword
+
+		# Make KeyVault
+		groupCreateCmd = 'az group create --name ' + self.keyVaultResourceGroup + ' --location ' + self.location
+		keyVaultCreateCmd = 'az keyvault create --name ' + self.keyVaultName + ' --resource-group ' + self.keyVaultResourceGroup
+		cmds = groupCreateCmd + ';' + keyVaultCreateCmd
+		subprocess.call(cmds, shell=True)
+
+		# KeyVault DNS Population 10 secs
+		keyVaultCreateValidationCmd = 'az keyvault show -n ' + self.keyVaultName + ' -g ' + self.keyVaultResourceGroup
+		subprocess.call(keyVaultCreateValidateCmd, shell=True)
+
+		# Create Self Signed Certificate
+		self.dnsName = self.clusterName + '.' + self.location + '.cloudapp.azure.com'
+		certificateCreateCmd = 'az keyvault certificate create --vault-name ' + self.keyVaultName + ' -p "$(az keyvault certificate get-default-policy)"'
+
+		subprocess.call(certificateCreateCmd, shell=True)
+
+		# Download Cert: az keyvault certificate download --vault-name <keyvaultname> --name <certname> --file <pem file name>
+		# Get Cert Properties: URL, URI, Thumbprint
+		# Convert Cert format for client connection: openssl pkcs12 -in ~/Downloads/<keyvaultname>-<certname>-YYYYMMDD.pfx -out ./<certname>.pem -nodes
+		# 
+		self.PFXCertificate = self.dnsName + '.pfx'
+
+		# Upload Certificate to KeyVault
+		uploadCertificateCmd = "az keyvault secret set --description '" + self.description + "' --encoding base64 --file ./" + self.PFXCertificate + " --name " + self.certificateName + " --vault-name " + self.keyVaultName
+
+		subprocess.call(uploadCertificateCmd, shell=True)
+
+		# Update Resource ID Parameter
+		resourceIdProcess = subprocess.Popen(["az", "keyvault", "show", "--name", self.keyVaultName, "--query", "id", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		stdout, stderr = resourceIdProcess.communicate()
+
+		self.parameters_file_json['parameters']['sourceVaultValue']['value'] = stdout.decode("utf-8")
+
+		# Update Thumbprint Parameter
+		thumbprintProcess = subprocess.Popen(["az", "keyvault", "secret", "--vault-name", self.keyVaultName, "--name", self.certificateName, "--query", "x509ThumbprintHex", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		stdout, stderr = thumbprintProcess.communicate()
+
+		self.parameters_file_json['parameters']['certificateThumbprint']['value'] = stdout.decode("utf-8")
+
+		# Update URL Parameter
+		urlProcess = subprocess.Popen(["az", "keyvault", "secret", "show", "--vault-name", self.keyVaultName, "--name", self.certificateName, "--query", "id", "-o", "tsv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		stdout, stderr = urlProcess.communicate()
+
+		self.parameters_file_json['parameters']['certificateUrlValue']['value'] = stdout.decode("utf-8")
+
+		# Update Parameters File
+		json.dump(self.parameters_file_json, open(self.parameters_file, 'w')) 
