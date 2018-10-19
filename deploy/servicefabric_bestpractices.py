7c3acb8ba1f14695eaea9c0d7e7858506fffcdd3
aljo-microsoft <37597129+aljo-microsoft@users.noreply.github.com>
2018-10-19 07:37:32 -0700

    Update servicefabric_bestpractices.py
    Additional Comments

--- deploy/servicefabric_bestpractices.py
+++ deploy/servicefabric_bestpractices.py
@@ -649,6 +649,12 @@ def main():
     db_password = az cosmosdb list-keys --name sfbpuser --resource-group sfbpdeployrg --query primaryMasterKey
     acr_password = az acr credential show -n sfbpacr --query passwords[0].value
     az container create -g sfbpdeployrg --name sfbpacr --image sfbpacr.azurecr.io/goservice:1.0.0 --registry-password $acr_password -e DATABASE_NAME="sfbpmongodb" DB_USER_NAME="sfbpuser" DB_PASSWORD=$db_password
+    - Import x509certificatename.pem into TrustedRoot Store and Client Credentials Store for Browsers
+    - On Windows Machine using Chrome used Portal To download PFX and ran following (don't forget to clear cookies if already trusted DNS with different cert):
+    - Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\My -FilePath ..\..\..\Downloads\sfbpkeyvault-x509certificatename-20181019.pfx
+    - Import-PfxCertificate -Exportable -CertStoreLocation Cert:\CurrentUser\TrustedPeople -FilePath ..\..\..\Downloads\sfbpkeyvault-x509certificatename-20181019.pfx
+    - SFX: https://sfbpcluster.westus.cloudapp.azure.com:19080/Explorer
+    - GoService: http://sfbpcluster.westus.cloudapp.azure.com:8081
     """
 
 if __name__ == '__main__':
