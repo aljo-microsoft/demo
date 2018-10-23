0e0e6edfde52b77c62590c972e225aaeebcabcef
aljo-microsoft <37597129+aljo-microsoft@users.noreply.github.com>
2018-10-23 15:46:25 -0700

    Update JavaService.java

--- build/javaservice/src/java/demo/JavaService.java
+++ build/javaservice/src/java/demo/JavaService.java
@@ -8,17 +8,15 @@ import java.sql.Statement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// Create SQL Table, Add Data, and Serve
-
 @RestController
 public class JavaService {
-    
+
     @RequestMapping("/")
     public String index() {
-
-        // Connection String
+                // Connection String
         String connectionUrl = "jdbc:sqlserver://sfbpsqlserver.database.windows.net:1433;database=sfbpdatabase;user=aljo@sfbpsqlserver;password=Password#1234;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
-        
+        String tableData = "SERVICETABLE\n";
+
         try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
             // Create Table
             String sqlCreateTable = "CREATE TABLE SFBPTABLE " +
@@ -33,20 +31,20 @@ public class JavaService {
             stmt.executeUpdate(sqlUser1Data);
             String sqlUser2Data = "INSERT INTO SERVICETABLE " +
                                    "VALUES('user2', 18006427676, 2)";
-            stmt.executeUpdate(sqlUser3Data);
+            stmt.executeUpdate(sqlUser2Data);
             // Query Table
             String SQL = "SELECT * FROM SERVICETABLE";
             ResultSet rs = stmt.executeQuery(SQL);
-            // Print Table Data
-            String tableData = "SERVICETABLE\n";
+            // Display Table Data
             while (rs.next()) {
                 tableData += "| " + rs.getString("name") + " | " + rs.getString("id") + " | \n";
             }
-            return tableData;
         }
         // Handle Exceptions
         catch (Exception e) {
             e.printStackTrace();
         }
+        return tableData;
     }
+
 }
